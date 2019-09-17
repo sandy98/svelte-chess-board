74528bf7689ecd3338f3cc2552893d168ef12c67
sandy98 <esavoretti@gmail.com>
2019-09-17 18:30:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svelte-chess-board
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/svelte-chess-board)
---
+++ index.js
@@ -0,0 +1,6 @@
+import ChessBoard from './components/Board.html';
+
+const chessBoard = new ChessBoard({
+  target: document.querySelector('#root'),
+  data: {}
+});
