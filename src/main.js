0abb77d57d89e181abd2bdd234b4774f837d80c2
sandy98 <esavoretti@gmail.com>
2019-11-07 00:17:06 -0300

    v. 0.14.5

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import ChessBoard from './ChessBoard.svelte';
 
+/* 
 const board = new ChessBoard({
 	target: document.body,
 	props: {
 	}
 });
 
-export default board;
+export default board; */
+
+export default ChessBoard
